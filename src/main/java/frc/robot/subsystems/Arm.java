38768a3f01be6b6881c7e3103529f0186631c239
RobbyJJr <robeastmond@gmail.com>
2023-01-18 15:02:18 -0700

    Began setting up arm PID controllers and encoders

--- src/main/java/frc/robot/subsystems/Arm.java
+++ src/main/java/frc/robot/subsystems/Arm.java
@@ -7,29 +7,48 @@ package frc.robot.subsystems;
 import frc.robot.Constants.CANBusIDs.ArmConstants;
 
 import com.revrobotics.CANSparkMax;
+import com.revrobotics.RelativeEncoder;
+import com.revrobotics.SparkMaxPIDController;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 
-import edu.wpi.first.wpilibj.motorcontrol.MotorControllerGroup;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class Arm extends SubsystemBase {
   // Set up four arm motors over CAN (Neo motor type is brushless)
-  CANSparkMax leftRotate = new CANSparkMax(ArmConstants.leftRotatorID, MotorType.kBrushless);
-  CANSparkMax rightRotate = new CANSparkMax(ArmConstants.rightRotatorID, MotorType.kBrushless);
-  CANSparkMax leftExtend = new CANSparkMax(ArmConstants.leftExtenderID, MotorType.kBrushless);
-  CANSparkMax rightExtend = new CANSparkMax(ArmConstants.rightExtenderID, MotorType.kBrushless);
+  CANSparkMax rotator = new CANSparkMax(ArmConstants.leftRotatorID, MotorType.kBrushless);
+  CANSparkMax rotatorFollower = new CANSparkMax(ArmConstants.rightRotatorID, MotorType.kBrushless);
+  CANSparkMax extender = new CANSparkMax(ArmConstants.leftExtenderID, MotorType.kBrushless);
+  CANSparkMax extenderFollower = new CANSparkMax(ArmConstants.rightExtenderID, MotorType.kBrushless);
 
-  MotorControllerGroup extender = new MotorControllerGroup(leftExtend, rightExtend);
-  MotorControllerGroup rotator = new MotorControllerGroup(leftRotate, rightRotate);
+  SparkMaxPIDController rotatorPID = rotator.getPIDController();
+  SparkMaxPIDController extenderPID = extender.getPIDController();
+
+  RelativeEncoder rotatorEncoder = rotator.getEncoder();
+  RelativeEncoder extenderEncoder = extender.getEncoder();  
+
+  /* Note to self: the encoder getPosition() method automatically returns in terms
+  *of revolutions, but it can also perform automatic conversion via the 
+  *setPositionConversionFactor() method. The PID controllers when set to position mode
+  *should use revolutions, but the documentation does not say for certain. Would be wise to
+  *test before attaching to robot
+  */
 
   /** Creates a new Arm. */
   public Arm() {
-    rightRotate.setInverted(true);
-    rightExtend.setInverted(true);
+    rotatorFollower.follow(rotator, true);
+    extenderFollower.follow(extender, true);
   }
 
   @Override
   public void periodic() {
     // This method will be called once per scheduler run
+  }
+
+  public void setRotatePosition(double position) {
+
+  }
+
+  public void setExtendPosition(double position) {
+
   }
 }
