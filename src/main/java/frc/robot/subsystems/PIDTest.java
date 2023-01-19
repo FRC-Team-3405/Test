ec4e9f52c7833ece7ed15ab7c9823dce8e627170
IsaacUtah1379 <122501927+IsaacUtah1379@users.noreply.github.com>
2023-01-18 22:20:17 -0700

    Create test subsystem for PID Controller test

---
+++ src/main/java/frc/robot/subsystems/PIDTest.java
@@ -0,0 +1,35 @@
+
+package frc.robot.subsystems;
+
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.SparkMaxPIDController;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+import com.revrobotics.CANSparkMax.ControlType;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class PIDTest extends SubsystemBase {
+
+  CANSparkMax test = new CANSparkMax(0, MotorType.kBrushless);
+  SparkMaxPIDController testPID = test.getPIDController();
+
+  /* Note to self: the encoder getPosition() method automatically returns in terms
+  *of revolutions, but it can also perform automatic conversion via the 
+  *setPositionConversionFactor() method. The PID controllers when set to position mode
+  *should use revolutions, but the documentation does not say for certain. Would be wise to
+  *test before attaching to robot
+  */
+
+  /** Creates a new PIDTest. */
+  public PIDTest() {
+  }
+
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+  }
+
+  public void setPosition(double position) {
+    testPID.setReference(position, ControlType.kPosition);
+  }
+}
