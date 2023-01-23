943c4378fd98ce561711f9fa824278d07602b827
RobbyJJr <robeastmond@gmail.com>
2023-01-23 13:16:13 -0700

    Removed unnecessary code

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -6,13 +6,10 @@ package frc.robot;
 
 import frc.robot.Constants.OperatorConstants;
 import frc.robot.commands.ArcadeDrive;
-import frc.robot.commands.PIDControllerTest;
 import frc.robot.commands.Autos;
 import frc.robot.commands.ExampleCommand;
 import frc.robot.subsystems.DriveTrain;
-import frc.robot.subsystems.PIDTest;
 import frc.robot.subsystems.ExampleSubsystem;
-import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import edu.wpi.first.wpilibj2.command.button.Trigger;
@@ -27,7 +24,6 @@ public class RobotContainer {
   // The robot's subsystems and commands are defined here...  
   private final ExampleSubsystem m_exampleSubsystem = new ExampleSubsystem();
   public final static DriveTrain driveTrain = new DriveTrain();
-  public final static PIDTest PIDTestSystem = new PIDTest();
   
   // Replace with CommandPS4Controller or CommandJoystick if needed
   public final static CommandXboxController m_driverController =
@@ -39,7 +35,6 @@ public class RobotContainer {
     configureBindings();
     // Default Command(s)
     driveTrain.setDefaultCommand(new ArcadeDrive());
-    PIDTestSystem.setDefaultCommand(new PIDControllerTest());
   }
 
   /**
